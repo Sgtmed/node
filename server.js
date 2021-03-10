ad09b40c3e41ba7276b0c7ca241e371061769003
Daniel Brost <danbrost@gmail.com>
2021-03-10 11:58:32 -0500

    add index.js file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function start() {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for" + pathname + " received");
         response.writeHead(200, { "Content-Tpe": "text / html" });
-        response.write(" Hello World!");
+        response.write(" Hello World!!!");
         response.end();
     }
     http.createServer(onRequest).listen(8888);
