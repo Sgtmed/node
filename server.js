0885224969c0ecd752e733a44eef61aca35b8fb8
Daniel Brost <danbrost@gmail.com>
2021-03-10 10:55:36 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+function onRequest(request, response) {
+    response.writeHead(200, { "Content-Tpe": "text / html" });
+    response.write(" Hello World");
+    response.end();
+}
+http.createServer(onRequest).listen(8888);
