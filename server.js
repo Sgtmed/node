ab2217412c637897690af38675c300110da8f581
Daniel Brost <danbrost@gmail.com>
2021-03-10 11:07:15 -0500

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 function onRequest(request, response) {
     response.writeHead(200, { "Content-Tpe": "text / html" });
-    response.write(" Hello World");
+    response.write(" Hello World!");
     response.end();
 }
 http.createServer(onRequest).listen(8888);
